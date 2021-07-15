71b0fc000702d52d15aef3ea4157efa034fbffe7
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-07-15 12:21:49 -0700

    Update RetroView.kt

--- app/src/main/java/com/draco/ludere/retroview/RetroView.kt
+++ app/src/main/java/com/draco/ludere/retroview/RetroView.kt
@@ -30,7 +30,7 @@ class RetroView(private val context: Context, compositeDisposable: CompositeDisp
              * The path to the ROM to load.
              * Example: /data/data/<package-id>/files/example.gba
              */
-            gameFilePath = "$storagePath/example.img"
+            gameFilePath = "$storagePath/example"
 
             /*
              * Direct ROM bytes to load.
