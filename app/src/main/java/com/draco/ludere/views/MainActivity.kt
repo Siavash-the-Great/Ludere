78d1bc2bf2729207fccff662ff2159589ca3a889
sohrab888000 <50212481+sohrab888000@users.noreply.github.com>
2021-07-15 12:21:01 -0700

    Update MainActivity.kt

--- app/src/main/java/com/draco/ludere/views/MainActivity.kt
+++ app/src/main/java/com/draco/ludere/views/MainActivity.kt
@@ -53,6 +53,27 @@ class MainActivity : AppCompatActivity() {
         send_email.visibility = View.INVISIBLE
         val relative = findViewById(R.id.relative) as RelativeLayout
         relative.setBackgroundResource(0)
+        
+        /*
+        new-> just copying
+        */
+        val storagePath: String = (this.getExternalFilesDir(null) ?: this.filesDir).path
+        InputStream in = getResources().openRawResource(R.raw.example)
+FileOutputStream out = new FileOutputStream(storagePath)
+byte[] buff = new byte[1024]
+int read = 0
+
+try {
+   while ((read = in.read(buff)) > 0) {
+      out.write(buff, 0, read)
+   }
+} finally {
+     in.close()
+     out.close()
+}
+        /*
+        new-> just copying
+        */
         val intent = Intent(this, GameActivity::class.java)
         startActivity(intent)
     }
@@ -82,5 +103,57 @@ class MainActivity : AppCompatActivity() {
         this@MainActivity.finish()
         exitProcess(0)
     }
+    
+    
+public void unzipFile(String targetPath, String zipFilePath) {  
+  
+      try {  
+          File zipFile = new File(zipFilePath);  
+          InputStream is = new FileInputStream(zipFile);  
+          ZipInputStream zis = new ZipInputStream(is);  
+          ZipEntry entry = null;  
+          System.out.println("Start decompression:" + zipFile.getName() + "...");  
+          while ((entry = zis.getNextEntry()) != null) {  
+              String zipPath = entry.getName();  
+              try {  
+  
+                  if (entry.isDirectory()) {  
+                      File zipFolder = new File(targetPath + File.separator  
+                              + zipPath);  
+                      if (!zipFolder.exists()) {  
+                          zipFolder.mkdirs();  
+                      }  
+                  } else {  
+                      File file = new File(targetPath + File.separator  
+                              + zipPath);  
+                      if (!file.exists()) {  
+                          File pathDir = file.getParentFile();  
+                          pathDir.mkdirs();  
+                          file.createNewFile();  
+                      }  
+  
+                      FileOutputStream fos = new FileOutputStream(file);  
+                      int bread;  
+                      while ((bread = zis.read()) != -1) {  
+                          fos.write(bread);  
+                      }  
+                      fos.close();  
+  
+                  }  
+                                     System.out.println("Successfully decompressed:" + zipPath);  
+  
+              } catch (Exception e) {  
+                                     System.out.println("Unzip" + zipPath + "Failed");  
+                  continue;  
+              }  
+          }  
+          zis.close();  
+          is.close();  
+                     System.out.println("decompression end");  
+      } catch (Exception e) {  
+          e.printStackTrace();  
+      }  
+  
+  }  
 
 }
