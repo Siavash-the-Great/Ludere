927f05b0f1a3b24e77f8e53afb6165a7d47ef866
Sohrab <86171882+SohrabIranpak@users.noreply.github.com>
2021-07-18 07:10:19 -0700

    Update MainActivity.kt

--- app/src/main/java/com/draco/ludere/views/MainActivity.kt
+++ app/src/main/java/com/draco/ludere/views/MainActivity.kt
@@ -59,7 +59,7 @@ class MainActivity : AppCompatActivity() {
 
     fun sendMsg(view: View) {
         val openURL = Intent(android.content.Intent.ACTION_VIEW)
-        openURL.data = Uri.parse("myket://comment?id=com.draco.ludere.gradius")
+        openURL.data = Uri.parse("myket://comment?id=com.draco.ludere.jackal")
         startActivity(openURL)
     }
 
@@ -74,7 +74,7 @@ class MainActivity : AppCompatActivity() {
 
     fun goToPage(view: View) {
         val openURL = Intent(android.content.Intent.ACTION_VIEW)
-        openURL.data = Uri.parse("myket://details?id=com.draco.ludere.gradius")
+        openURL.data = Uri.parse("myket://details?id=com.draco.ludere.jackal")
         startActivity(openURL)
     }
 
