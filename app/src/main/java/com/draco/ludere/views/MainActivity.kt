271f78d932fc813ad028dd0fa413737fb0caebfd
Siavash-the-Great <59716718+Siavash-the-Great@users.noreply.github.com>
2021-08-08 18:15:38 -0700

    Update MainActivity.kt

--- app/src/main/java/com/draco/ludere/views/MainActivity.kt
+++ app/src/main/java/com/draco/ludere/views/MainActivity.kt
@@ -25,9 +25,12 @@ import io.reactivex.disposables.CompositeDisposable
 import kotlinx.coroutines.*
 import ir.tapsell.sdk.bannerads.TapsellBannerType
 import ir.tapsell.sdk.bannerads.TapsellBannerView
+import ir.tapsell.sdk.*
+import ir.tapsell.sdk.TapsellAdRequestOptions.CACHE_TYPE_STREAMED
 
 
 class MainActivity : AppCompatActivity() {
+    var ad: TapsellAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +41,10 @@ class MainActivity : AppCompatActivity() {
     /** Called when the user touches the button */
     fun start(view: View) {
         // Do something in response to button click
-                    startActivity(Intent(this@MainActivity, InterstitialActivity::class.java))
+         requestInterstitialBannerAd(AdType.BANNER) 
+         requestInterstitialBannerAd(AdType.VIDEO)
+         showAd()
+               //     startActivity(Intent(this@MainActivity, InterstitialActivity::class.java))
   /*                  
         val start_the_game_button = findViewById(R.id.start_the_game_button) as Button
         start_the_game_button.isEnabled = false
@@ -88,4 +94,64 @@ class MainActivity : AppCompatActivity() {
         exitProcess(0)
     }
 
+    
+        private fun requestInterstitialBannerAd(type: AdType) {
+        val options = TapsellAdRequestOptions(CACHE_TYPE_STREAMED)
+        Tapsell.requestAd(this@MainActivity,
+            if (type == AdType.BANNER) "610ed8bc35114c6ff3a596ee" else
+                "610ecc7d260bc85635a14601", options,
+            object : TapsellAdRequestListener() {
+                override fun onAdAvailable(ad: TapsellAd?) {
+                    if (isDestroyed)
+                        return
+
+                    this@MainActivity.ad = ad
+                    btnShowAd.isEnabled = true
+                }
+
+                override fun onExpiring(ad: TapsellAd?) {
+                    TODO("not implemented")
+                }
+
+                override fun onNoAdAvailable() {
+                    TODO("not implemented")
+                }
+
+                override fun onError(str: String?) {
+                    TODO("not implemented")
+                }
+
+                override fun onNoNetwork() {
+                    TODO("not implemented")
+                }
+            })
+    }
+
+    private fun showAd() {
+        ad?.let {
+            val showOptions = TapsellShowOptions()
+            showOptions.rotationMode = TapsellShowOptions.ROTATION_LOCKED_LANDSCAPE
+            it.show(this@MainActivity, showOptions, object : TapsellAdShowListener() {
+                override fun onOpened(ad: TapsellAd) {
+                    Log.e("InterstitialActivity", "on ad opened")
+                }
+
+                override fun onClosed(ad: TapsellAd) {
+                    Log.e("InterstitialActivity", "on ad closed")
+                }
+            })
+        } ?: run {
+            Log.e("InterstitialActivity", "ad is not available")
+        }
+
+        btnShowAd.isEnabled = false
+        ad = null
+    }
+}
+
+
+    
+}
+enum class AdType {
+    BANNER, VIDEO
 }
