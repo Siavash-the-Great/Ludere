7c347eee7edc3c41dcb66e806d91861da4a407a0
Siavash Iranpak <59716718+Siavash-the-Great@users.noreply.github.com>
2022-02-12 20:02:09 -0800

    Update RetroView.kt

--- app/src/main/java/com/draco/ludere/retroview/RetroView.kt
+++ app/src/main/java/com/draco/ludere/retroview/RetroView.kt
@@ -20,7 +20,11 @@ class RetroView(private val context: Context, compositeDisposable: CompositeDisp
 
     private val retroViewData = GLRetroViewData(context).apply {
         coreFilePath = "libcore.so"
-        gameFileBytes = context.resources.openRawResource(R.raw.rom).use { it.readBytes() }
+       // gameFileBytes = context.resources.openRawResource(R.raw.rom).use { it.readBytes() }
+            gameFilePath = "$storagePath/example.sfc"
+            gameFileBytes = null
+            /* (Optional) System directory */
+            systemDirectory = storagePath + "/system"
         shader = GLRetroView.SHADER_SHARP
         variables = getCoreVariables()
 
